fed7f51519fbf491217799d17db9f9d7a63fa9b9
bettercallmk <bettercallmk98@gmail.com>
2025-03-03 19:56:18 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -1,10 +1,11 @@
 function solution(num1, num2) {
-    let answer = 0;
+    var answer = 0;
     
-    if (num1 == num2) {
+    if (num1 === num2) {
         answer = 1;
     } else {
         answer = -1;
     }
+    
     return answer;
 }
