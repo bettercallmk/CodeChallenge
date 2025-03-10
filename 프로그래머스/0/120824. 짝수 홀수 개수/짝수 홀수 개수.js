3685211ef30df64d2715cb2803c0ae8c47162be1
bettercallmk <bettercallmk98@gmail.com>
2025-03-10 22:29:45 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+    var answer = [];
+    let even = 0;
+    let odd = 0;
+    for(num of num_list) {
+        if (num %2 == 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
+    
+    answer.push(even);
+    answer.push(odd);
+    return answer;
+}
