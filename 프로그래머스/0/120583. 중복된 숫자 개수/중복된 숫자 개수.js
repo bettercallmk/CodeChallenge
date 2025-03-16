1d12e7464a92f1d2a9556762088f8f5ba8c626b5
bettercallmk <bettercallmk98@gmail.com>
2025-03-16 19:25:52 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.js
@@ -1,11 +1,3 @@
 function solution(array, n) {
-    var answer = 0;
-    
-    for(let i=0; i<array.length; i++) {
-        if (array[i] === n) {
-            answer ++;
-        }
-    }
-    
-    return answer;
+    return array.filter( num => num === n).length;
 }
