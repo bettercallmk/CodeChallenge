0ee1a35896dc9cf94c4cbf3be3f99cffe0ffde8a
bettercallmk <bettercallmk98@gmail.com>
2025-03-20 14:50:24 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,22 +1,5 @@
 function solution(hp) {
-    
-    let rem = hp;
-    let answer = 0;
-    
-    if (rem % 5 >= 0) {
-        answer += (parseInt(rem / 5));
-        rem = parseInt(rem % 5);
 
-    }
     
-    if (rem % 3 >= 0) {
-        answer += (parseInt(rem / 3));
-        answer += (parseInt(rem % 3));
-        rem = parseInt(rem % 3);
-
-    }
-    
-    
-    
-    return answer;
+    return Math.floor(hp/5) + Math.floor(hp%5/3) + Math.floor(hp%5%3);
 }
