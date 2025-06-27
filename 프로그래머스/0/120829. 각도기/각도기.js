404b3f29d37ca90534c94c85d89a3c960080c7e0
bettercallmk <bettercallmk98@gmail.com>
2025-06-27 21:21:11 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -3,17 +3,11 @@ function solution(angle) {
     
     if (0 < angle && angle < 90) {
         answer = 1;
-    }
-    
-    if (angle == 90) {
+    } else if (angle == 90) {
         answer = 2;
-    }
-    
-    if (90 < angle && angle < 180) {
+    } else if (90 < angle && angle < 180) {
         answer = 3;
-    }
-    
-    if (angle == 180) {
+    } else if (angle == 180) {
         answer = 4;
     }
     
