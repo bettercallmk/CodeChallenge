7ac707a21e33c58ff676592cb8bf25de7c9f3cb5
bettercallmk <bettercallmk98@gmail.com>
2025-06-27 21:27:30 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.js
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.js
@@ -1,12 +1,7 @@
 function solution(n, k) {
-    let answer = 0;
-    let discount = 0;
+    var answer = (n * 12000) + (k * 2000) - (parseInt(n / 10) * 2000);
     
-    answer += n * 12000;
-    answer += k * 2000;
     
-    if (n>=10) {
-        discount += (parseInt(n/10) * 2000);
-    }
-    return answer-discount;
+    
+    return answer;
 }
